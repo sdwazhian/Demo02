1737ceb61761ca0dc0b23776d61c6a33a114f3e5
sdwangzewen <sdwangzewen@163.com>
2019-06-27 16:26:21 +0800

    添加6.7

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World555");
+        System.out.println("Hello World666");
+        System.out.println("Hello World777");
     }
 }
